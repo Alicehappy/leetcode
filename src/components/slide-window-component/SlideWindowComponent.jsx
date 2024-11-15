56e4ce64614c93efb562242fd77d10d16c645f6d
Alice Zou <Alicehappy@users.noreply.github.com>
2024-11-15 09:51:26 -0500

    Solve slide window challenge

---
+++ src/components/slide-window-component/SlideWindowComponent.jsx
@@ -0,0 +1,33 @@
+import "./SlideWindowComponent.scss";
+
+function SlideWindowComponent() {
+  const maxVowels = function (s, k) {
+    const vowels = ["a", "e", "i", "o", "u"];
+
+    let maxVowelsCount = 0;
+
+    let currentVowelsCount = 0;
+
+    for (let i = 0; i < k; i++) {
+      if (vowels.includes(s[i])) {
+        currentVowelsCount++;
+      }
+    }
+
+    maxVowelsCount = currentVowelsCount;
+
+    for (let i = k; i < s.length; i++) {
+      if (vowels.includes(s[i - k])) currentVowelsCount--;
+      if (vowels.includes(s[i])) currentVowelsCount++;
+
+      maxVowelsCount = Math.max(maxVowelsCount, currentVowelsCount);
+    }
+
+    return maxVowelsCount;
+  };
+
+  console.log(maxVowels("adisegs", 2));
+  return <div></div>;
+}
+
+export default SlideWindowComponent;
