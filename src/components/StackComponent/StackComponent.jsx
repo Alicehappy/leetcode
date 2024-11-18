246470beae846c911965f408744f84e0da3f9862
Alice Zou <Alicehappy@users.noreply.github.com>
2024-11-18 14:45:17 -0500

    Improve code by simplify the logic using while loop.

--- src/components/StackComponent/StackComponent.jsx
+++ src/components/StackComponent/StackComponent.jsx
@@ -3,68 +3,37 @@ import "./StackComponent.scss";
 function StackComponent() {
 
     // Nov 17, 2024, Stack: Asteroids collision (Medium)
+    // This could be improved by the faster solution in leetcode
 
     const asteroidCollision = function (asteroids) {
         let res = [];
-        let isEqual = false;
     
-        for (let i = 0; i < asteroids.length; i++) {
-            if (res.length === 0) {
-                res.push(asteroids[i])
-            } else {
-                if (res[res.length - 1] > 0 && asteroids[i] > 0
-                    || res[res.length - 1] < 0 && asteroids[i] < 0
-                    || res[res.length - 1] < 0 && asteroids[i] > 0
-                ) {
-                    res.push(asteroids[i]);
-                } else {
-                    if ((res[res.length - 1] + asteroids[i]) < 0) {
-                        res.pop();
-                        for (let j = res.length - 1; j >= 0; j--) {
-                            if (res[j] > 0 && asteroids[i] > 0
-                                || res[j] < 0 && asteroids[i] < 0
-                                || res[j] < 0 && asteroids[i] > 0
-                            ) {
-                                res.push(asteroids[i]);
-                                break;
-                            } else {
-                                if ((res[j] + asteroids[i]) < 0) {
-                                    res.pop();
-                                } else if ((res[j] + asteroids[i]) === 0) {
-                                    isEqual = true;
-                                    res.pop();
-                                    break;
-                                } else {
-                                    break;
-                                }
-    
-                            }
-    
-    
-                        }
-    
-                        if (res.length === 0) {
-                            if (!isEqual) {
-                                res.push(asteroids[i]);
-                            }
-                            isEqual = false;
-                            continue;
-                        }
-    
-    
-                    }
-    
-                    else if ((res[res.length - 1] + asteroids[i]) === 0) {
-                        res.pop();
-    
-                    }
-                }
-            };
-    
-    
+    for ( let i = 0; i < asteroids.length; i++) {
+
+        while(res.length > 0 && res[res.length - 1] > 0 && asteroids[i] < 0) {
+            let top = res[res.length -1];
+            
+            if(top + asteroids[i] > 0) {
+                asteroids[i] = 0;
+                break;
+            }
+            if(top + asteroids[i] == 0) {
+                res.pop();
+                asteroids[i] = 0;
+                break;
+            }
+            if(top + asteroids[i] < 0) {
+                res.pop();
+            }
         }
+        
+        if(asteroids[i] !== 0) {
+            res.push(asteroids[i]);
+        }
+
+    }
     
-        return res;
+    return res;
     };
 
     console.log(asteroidCollision([-2, 1, -2, -2]));
