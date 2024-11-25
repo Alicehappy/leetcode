b10de946b481d3b681d0a66239ebddb67494b377
Alice Zou <Alicehappy@users.noreply.github.com>
2024-11-25 13:14:10 -0500

    Sliding window variance, which needs to satisfy certain condition.

--- src/components/SlideWindowComponent/SlideWindowComponent.jsx
+++ src/components/SlideWindowComponent/SlideWindowComponent.jsx
@@ -29,6 +29,33 @@ function SlideWindowComponent() {
   };
 
   console.log(maxVowels("adisegs", 2));
+
+  // sliding window variance: condition
+  var longestOnes = function (nums, k) {
+    let max = 0,
+      left = 0,
+      counter = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+      if (nums[right] === 0) {
+        counter++;
+      }
+
+      while (counter > k) {
+        if (nums[left] == 0) {
+          counter = counter - 1;
+        }
+        left++;
+      }
+
+      max = Math.max(max, right - left + 1);
+    }
+
+    return max;
+  };
+
+  console.log(longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2));
+
   return <div></div>;
 }
 
